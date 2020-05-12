1382f14221c928bb50771b44ac63e60aa93fe476
DenisSeroshtan <haron_777@mail.ru>
2020-05-12 23:17:18 +0500

    get token localStorage and guard fake token with axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created () {
+    const userStringToken = localStorage.getItem('user')
+    if (userStringToken) {
+      const userTokenData = JSON.parse(userStringToken)
+      this.$store.commit('SET_USER_DATA', userTokenData)
+    }
+
+    // guard fake token 
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return new Promise(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
